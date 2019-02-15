f994614f4f7159b9c849a479367d5f7d12375d13
Azamat <kirashuffler@gmail.com>
2019-02-15 17:23:19 +0500

    Add files via upload
    Добавлен поиск конца отрезка и старт алгоритма из из этого конца

--- recur_chain_approx.py
+++ recur_chain_approx.py
@@ -11,39 +11,64 @@ def scanBorder_recc(img, start, prev_dir, res):
                    (prev_dir + 6) % 8, (prev_dir + 3) % 8, (prev_dir + 5) % 8, (prev_dir + 4) % 8]
     y = start[0]
     x = start[1]
-    print(start)
     direction = -1
+
     for i in dirs:
-        if img[y + change_y[i]][x + change_x[i]] == 255:
+        next_pt_value = img[y + change_y[i]][x + change_x[i]]
+        if next_pt_value:
+            img[y][x] = 0
             direction = i
+            res.append(direction)
+
             break
+        #elif next_pt_value == 1:
+        #    img[y][x] = 0
+        #    direction = i
+        #    break
     if direction == -1:
         return
-    res.append(direction)
-    img[y][x] = 0
+
     y += change_y[direction]
     x += change_x[direction]
     scanBorder_recc(img, [y, x], direction, res)
 
 
 def scanBorder(img, start):
+    ls = line_start(img, start)
+    print(ls)
     res = []
-    y = start[0]
-    x = start[1]
-
+    y = ls[0]
+    x = ls[1]
     direction = -1
     for dir in range(8):
-        if img[y + change_y[dir]][x + change_x[dir]] == 255:
+        if img[y + change_y[dir]][x + change_x[dir]]:
             direction = dir
             break
-
     y += change_y[direction]
     x += change_x[direction]
-    print([y,x])
     res.append(direction)
     scanBorder_recc(img, [y, x], direction, res)
     img[y][x] = 0
-    return res
+    return [ls, res]
+
+def line_start(img, start):
+    y = start[0]
+    x = start[1]
+    direction = -1
+    while True:
+        dirs = [direction, (direction + 1) % 8, (direction + 7) % 8, (direction + 2) % 8,
+                (direction + 6) % 8, (direction + 3) % 8, (direction + 5) % 8, (direction + 4) % 8]
+        for i in dirs:
+            next_pt_value = img[y + change_y[i]][x + change_x[i]]
+            if next_pt_value == 255:
+                print([y, x])
+                img[y][x] = 1
+                direction = i
+                y += change_y[direction]
+                x += change_x[direction]
+                break
+            elif next_pt_value == 1:
+                return [y, x]
 
 def find_start(img):
     start_pt = (0, 0)
@@ -54,9 +79,9 @@ def find_start(img):
             break
     return start_pt
 
-def draw_by_chain(img, start, chain):
-    y = start[0]
-    x = start[1]
+def draw_by_chain(img, start_pt, chain):
+    y = start_pt[0]
+    x = start_pt[1]
     #print(chain)
     for dir in chain:
         #print(dir)
@@ -77,6 +102,6 @@ cv2.imwrite('edges.png', edges)
 contour = np.zeros((h, w, 3), np.uint8)
 start = find_start(img)
 codes = scanBorder(img, start)
-draw_by_chain(contour, start, codes)
-cv2.imwrite("cntr.png", contour)
 print(codes)
+draw_by_chain(contour, codes[0], codes[1])
+cv2.imwrite("cntr.png", contour)
